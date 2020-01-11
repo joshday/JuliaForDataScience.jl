29e5f618659c8d2e01f0289fb1547a74a014f0b9
zeptodoctor <44736852+zeptodoctor@users.noreply.github.com>
2020-01-11 20:30:21 +0000

    build based on 3302886

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"00-home/#Julia-For-Data-Science-1","page":"Home","title":"Julia For Data Science","text":"","category":"section"},{"location":"01-intro/#Introduction-1","page":"01 - Introduction","title":"Introduction","text":"","category":"section"},{"location":"01-intro/#Welcome-1","page":"01 - Introduction","title":"Welcome","text":"","category":"section"},{"location":"02-dataviz/#Data-Visualization-1","page":"02 - Data Visualization","title":"Data Visualization","text":"","category":"section"}]
+}
